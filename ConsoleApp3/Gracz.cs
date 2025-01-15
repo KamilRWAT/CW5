fc8eb107cfc5fc8e794aa25dd810a4efaf9d888f
WML25\kamil.cieplak <kam.cieplak@wp.pl>
2025-01-15 08:40:48 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp3/Gracz.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Gracz
+    {
+        public string Nick { get; set; }
+        public string Haslo { get; set; }
+        public int saldo { get; set; }
+
+        //public string PelneDane => $"{Nick}%{Haslo}%{saldo}";
+        private string pelneDane;
+
+        public string PelneDane
+        {
+            get { //return pelneDane = $"{Nick}%{Haslo}%{saldo}";
+                    return pelneDane = string.Join('%',Nick,Haslo,saldo);
+                }
+            set { pelneDane = value; }
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleApp3;
+using System.Text;
+
+string path = "./test.txt";
+Gracz gracz1 = new Gracz();
+gracz1.Nick = "Tomek";
+gracz1.Haslo = "Tomek123";
+gracz1.saldo = 100;
+
+Gracz gracz2 = new Gracz();
+gracz2.Nick = "Piotr";
+gracz2.Haslo = "Piotr12";
+gracz2.saldo = 200;
+
+Gracz gracz3 = new Gracz();
+gracz3.Nick = "Adam";
+gracz3.Haslo = "Adam12";
+gracz3.saldo = 300;
+
+List<Gracz> gracze = new List<Gracz>();
+gracze.Add(gracz1);
+gracze.Add(gracz2);
+gracze.Add(gracz3);
+
+List<string> list = new List<string>();
+foreach (Gracz g in gracze)
+{
+    list.Add(g.PelneDane);
+    //Console.WriteLine(g.PelneDane);
+}
+
+File.WriteAllLines(path,list.ToArray());
